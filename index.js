a349ad23ad4063bdb04a24aaba884c7314d31de7
PillGP <pillgp@icloud.com>
2018-07-15 15:01:53 +0100

    res.json() seems to be better than stringifying JSON

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')()
 const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-  res.send(JSON.stringify({ error: false, code: 200, message: 'It\'s alive!' }))
+  res.json({ error: false, code: 200, message: 'It\'s alive!' })
 })
 
 app.listen(port)
