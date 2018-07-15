9302dd3ca6c2f36251991a6c70875c10d291cdfe
PillGP <pillgp@icloud.com>
2018-07-15 14:58:57 +0100

    forgot a tiny =>, fuck

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 const port = process.env.PORT || 8000
 
-app.get('/', (req, res) {
+app.get('/', (req, res) => {
   res.send(JSON.stringify({ error: false, code: 200, message: 'It\'s alive!' }))
 })
 
